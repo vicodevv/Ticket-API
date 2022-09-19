1b689fd04859f84729d8b49f74f9445932bb69e4
Victor Omoniyi <vomoniyi2001@gmail.com>
2022-09-19 19:44:21 +0100

    user schema

---
+++ models/user.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+  firstname: {
+    type: String,
+    required: true,
+  },
+  lastname: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  timestamps: true,
+  }
+);
+ 
