a049f583401d89fb153ec0412f63f5641782b598
Victor Omoniyi <vomoniyi2001@gmail.com>
2022-09-22 18:33:58 +0100

    test

--- tests/user.test.js
+++ tests/user.test.js
@@ -3,3 +3,12 @@ import User from '../models/User'; // import the model
 import mongoose from 'mongoose';
 
 const app = createServer(); // create the server
+
+describe ('Users', () => {
+    beforeEach((done) =>{
+        mongoose.connection.collections.users.drop(() => {
+            done();
+        })
+    })
+
+})
